7f14a7bec50c3d4014be62748f31a45dbc0e19bd
gongsi <80491636@qq.com>
2019-11-01 17:19:29 +0800

    今天第一天上线。更新了网站关键字。

--- public/index.php
+++ public/index.php
@@ -13,6 +13,6 @@
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
-define('SITE_URL', 'http://perichina.cn/MN');
+define('SITE_URL', 'https://perichina.cn/MN');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
